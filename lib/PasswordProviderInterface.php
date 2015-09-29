326ddf73f6dceec828e0d4abe66acaa03c15eab5
Nigel Greenway <nigel_greenway@me.com>
2015-09-29 22:29:52 +0100

    Tidy up of line endings

--- lib/PasswordProviderInterface.php
+++ lib/PasswordProviderInterface.php
@@ -4,7 +4,7 @@ namespace Tutis;
 
 /**
  * Interface for a password provider
- * 
+ *
  * @package Tutis
  * @author  Nigel Greenway <nigel_greenway@me.com>
  */
@@ -12,10 +12,10 @@ interface PasswordProviderInterface
 {
     /**
      * Generate the hash from the raw password
-     * 
+     *
      * @param string $password
      * @param array  $options
-     * 
+     *
      * @return self
      */
     public static function hash($password, array $options = []);
@@ -26,13 +26,15 @@ interface PasswordProviderInterface
      * @param string $password
      * @param string $hash
      *
+     * @throw InvalidPasswordException
+     *
      * @return bool
      */
     public static function verify($password, $hash);
 
     /**
      * Get the hashed password
-     * 
+     *
      * @return string
      */
     public function getHash();
